65e3bc1583a08100f291187e0d7487ed2c6b02db
Peter <piowiedlocha@gmail.com>
2025-04-18 12:00:50 +0200

    JDP250301-12 Product - encja dodana; do dodania - relacje

---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,30 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "product_id")
+    private Long product_id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "price")
+    private Float price;
+
+    @Column(name = "description")
+    private String description;
+
+
+    private Long group_id;
+
+}
