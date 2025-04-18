f87dff597ad5d06ab945bc40380d7e076839f215
Peter <piowiedlocha@gmail.com>
2025-04-18 21:47:43 +0200

    changed to JpaRepository

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -2,10 +2,11 @@ package com.kodilla.ecommercee.repository;
 
 
 import com.kodilla.ecommercee.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
