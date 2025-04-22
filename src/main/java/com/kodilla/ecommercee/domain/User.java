bbacdae05ad690b6b4927632bdd4a0797dd92c55
Robert Litwiński <robert.lit97@gmail.com>
2025-04-22 11:14:10 +0200

    JDP250301-15: Dodano encje User i Repozytorium

---
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -0,0 +1,37 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "is_blocked", nullable = false)
+    private boolean isBlocked;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
+
+}
