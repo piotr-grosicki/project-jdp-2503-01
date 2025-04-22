79d8e4b1a2989f56a00546b61f675e5d5e0a0072
Robert Litwiński <robert.lit97@gmail.com>
2025-04-22 10:52:27 +0200

    JDP250301-11: Dodano Encję Order i Repozytorium

---
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -0,0 +1,34 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @Column(name = "order_date")
+    private LocalDate orderDate;
+
+    @Column(length = 50)
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+}
