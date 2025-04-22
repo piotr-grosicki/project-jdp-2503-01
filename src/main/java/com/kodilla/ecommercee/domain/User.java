5aa8df5e00452d3baf3915f24946bf9e77c35c5b
Peter <piowiedlocha@gmail.com>
2025-04-22 22:54:56 +0200

    Update User.java

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,10 +1,7 @@
 package com.kodilla.ecommercee.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 
